75d7215e65181a18275a0923b440005fcaaa7f68
arcsinecosine <34241521+arcsinecosine@users.noreply.github.com>
2017-12-04 17:22:20 -0500

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,21 @@
+class world:
+    def __init__(self,time):
+        self.time = time
+        
+class player:
+    class test:
+        def __init__(self):
+            self.hp = 100
+            
+    def __init__(self,name):
+        self.name = name
+
+
+x = player("ben")
+
+print (x.name)
+print (x.test.hp)
+
+
+
+
